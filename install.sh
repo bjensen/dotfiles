bcbc97bbe837f4ad507a174e66baf1bb48687a51
Brian Jensen <briajens@gmail.com>
2016-12-24 14:56:05 +0100

    done upgrading dotfiles

--- install.sh
+++ install.sh
@@ -19,14 +19,12 @@ echo "done"
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory ..."
 cd $dir
+
+for file in $files; do
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
+done
+
 echo "done"
 
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
 
